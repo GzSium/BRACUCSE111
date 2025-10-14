29b1eb850b0819467079435248a2a126b64dcc3d
GzSium <168644381+GzSium@users.noreply.github.com>
2025-10-14 21:39:10 +0600

    Create Task 2.java

---
+++ Assignment 7/Task 2.java
@@ -0,0 +1,12 @@
+//Task2
+public class CheckingAccount extends Account{
+    public static int count = 0;
+    public CheckingAccount(){
+       super(0.0);
+       count++;
+    }
+    public CheckingAccount(double balance){
+        super(balance);
+        count++;
+    }
+
